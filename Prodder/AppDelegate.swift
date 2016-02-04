75f0e9b3907b63ed596fb6615cdea745a4e2f293
Hamilton Chapman <hamchapman@gmail.com>
2016-02-04 11:40:40 +0000

    Initial Commit

---
+++ Prodder/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Prodder
+//
+//  Created by Hamilton Chapman on 04/02/2016.
+//  Copyright © 2016 hc.gg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
